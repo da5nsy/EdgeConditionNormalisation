4f526745ed555720759e1f6cebfc60c1973abb49
da5nsy <dannygarside@outlook.com>
2022-05-17 12:05:33 -0400

    change plotting

--- EdgeConditionNormalisation.m
+++ EdgeConditionNormalisation.m
@@ -26,25 +26,25 @@ end
 
 plot([gamut(:,1);gamut(1,1)],[gamut(:,2);gamut(1,2)],'k')
 scatter(grid(:,1),grid(:,2),'ro','MarkerEdgeAlpha',0.1)
-scatter(grid_in(:,1),grid_in(:,2),'kx')
+scatter(grid_in(:,1),grid_in(:,2),'k.')
 
 %% Generate responses without bias
 
-noiseVal = 0.1;
+noiseVal = 0.01;
 responses = cues + randn(size(cues))*noiseVal;
 
 scatter(responses(:,1),responses(:,2),'b.')
 
 % add biases
 % find closest attractor
-% modify response such that
+% modify response such that it is affected in some way by the attractor
 
 % pull responses back inside response space
 for i = 1:size(responses,1)
     in(i) = inpolygon(responses(i,1), responses(i,2),...
         gamut(:,1), gamut(:,2));
     if ~in(i)
-        [d_min(i), response_in_x(i), response_in_y(i)] = p_poly_dist(responses(i,1), responses(i,2),...
+        [~, response_in_x(i), response_in_y(i)] = p_poly_dist(responses(i,1), responses(i,2),...
             gamut(:,1), gamut(:,2),1);
     else
         response_in_x(i) = responses(i,1);
@@ -55,10 +55,20 @@ end
 responses_in = [response_in_x; response_in_y];
  
 scatter(response_in_x,response_in_y,'g.')
-quiver(responses(~in,1),responses(~in,2),...
-    response_in_x(~in)'-responses(~in,1),...
-    response_in_y(~in)'-responses(~in,2),...
-    'off') % scaling off
+
+% quiver(responses(~in,1),responses(~in,2),...
+%     response_in_x(~in)'-responses(~in,1),...
+%     response_in_y(~in)'-responses(~in,2),...
+%     'off') % scaling off
+
+quiver(cues(:,1),cues(:,2),...
+    response_in_x'-cues(:,1),...
+    response_in_y'-cues(:,2),...
+    'AutoScale','off','ShowArrowHead','off') % scaling off
+
+%% Plot mean vectors
+
+%% Normalise (additive/multiplicative?)
 
 
 
